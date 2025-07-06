c12b0fb93b764b511204b2e27d308c9ba41500c1
Thế Anh DevPM <theanhdev369@gmail.com>
2025-07-06 10:52:16 +0700

    feat(routes, views): thêm vào views welcome, product, sửa home để hiển thị giao diện và khai báo các  routes tương ứng

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return view('home');
+});
+
+// Tạo Route mới
+Route::get('/unicode', function() {
+    return view('home');
+});
+
+Route::get('/san-pham', function() {
+    return view('product');
+    //  Lưu ý: chỉ lấy tên của các file trước .blade.php khi return view('')
+    //  Ví dụ trong thư mục views có home.blade.php, welcome.blade.php,..
+    //  Thì ta lấy home, welcome,... cho vào trong return view('welcome'), view('welcome),..
+    //  Tránh dùng sai như sau: returm view('home.blade') hay return view('welcome.blade.php'),...
 });
